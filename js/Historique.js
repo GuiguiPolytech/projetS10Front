8f5c091d91b5fa3a6a8c4d64f6f2f4de9efd74da
GuiguiPolytech <guillaumemacie@gmail.com>
2023-05-10 13:56:47 +0200

    details

--- js/Historique.js
+++ js/Historique.js
@@ -1,27 +1,21 @@
-const tableBody = document.querySelector("#table-historique tbody");
+const tableHistorique = document.getElementById("table-historique").getElementsByTagName('tbody')[0];
 
 fetch("http://localhost:8080/date-etat")
   .then(response => response.json())
   .then(data => {
-    // Parcourir les données et créer chaque ligne du tableau
-    data.forEach(etat => {
-      const row = document.createElement("tr");
-
-      const etatIdCell = document.createElement("td");
-      etatIdCell.textContent = etat.etat.id;
-      row.appendChild(etatIdCell);
-
-      const dateEtatCell = document.createElement("td");
-      dateEtatCell.textContent = new Date(etat.dateEtat).toLocaleDateString();
-      row.appendChild(dateEtatCell);
-
-      const heureEtatCell = document.createElement("td");
-      heureEtatCell.textContent = etat.heureEtat;
-      row.appendChild(heureEtatCell);
-
-      tableBody.appendChild(row);
+    console.log(data);
+    data.forEach(item => {
+      const row = tableHistorique.insertRow();
+      const etatIdCell = row.insertCell(0);
+      const libelleCell = row.insertCell(1);
+      const dateEtatCell = row.insertCell(2);
+      const heureEtatCell = row.insertCell(3);
+      etatIdCell.innerHTML = item.etat.id;
+      libelleCell.innerHTML = item.etat.libelle;
+      dateEtatCell.innerHTML = new Date(item.dateEtat).toLocaleDateString();
+      heureEtatCell.innerHTML = item.heureEtat;
     });
   })
   .catch(error => {
-    console.error("Erreur lors de la récupération des données", error);
+    console.error("Erreur lors de la récupération de l'historique", error);
   });
